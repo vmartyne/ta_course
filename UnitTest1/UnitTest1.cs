016dbef1a13e1ca4c977b7bac3d01023fb71695e
Volodymyr Martynenko <vmart@engineer.com>
2023-06-12 13:59:39 +0300

    adjusted unit tests

--- UnitTest1/UnitTest1.cs
+++ UnitTest1/UnitTest1.cs
@@ -9,18 +9,38 @@ public class Tests
     public void Setup()
     {
         person = new Person();
+        person.FirstName = "John";
+        person.LastName = "Doe";
     }
 
     [Test]
-    public void Test1()
+    public void TestFirstNameIsJohn()
     {
-        Assert.That(1, Is.EqualTo(2));
+        Assert.That(person.FirstName, Is.EqualTo("Joe")); // Check if Name is Joe
     }
 
     [Test]
-    public void Test2()
+    public void TestFirstNameValidation()
     {
-        Assert.Throws<ArgumentException>(() => person.FirstName = "Name");
+        Assert.Throws<ArgumentException>(() => person.FirstName = "1234"); // Check if First name cannot be digits
+        Assert.Throws<ArgumentException>(() => person.FirstName = ""); // Check if First name cannot be empty
+        Assert.Throws<ArgumentException>(() => person.FirstName = "Qwertyuioplkjhgfdsazxcvbnmkloyt"); // Check if First name cannot be more then 30 charecters
+        Assert.Throws<ArgumentException>(() => person.FirstName = "Пилип"); // Check if First name cannot be cyrillic
     }
+
+    [Test]
+    public void TestLastNameIsDoe()
+    {
+        Assert.That(person.LastName, Is.EqualTo("Doe")); // Check if Surname is Doe
+    }
+    /*
+    [TestCase("", true)]
+    [TestCase("Qwertyuioplkjhgfdsazxcvbnmkloyt", true)]
+
+    public void TestLastNameValidation(string testData, bool expected)
+    {
+        Assert.That(person.LastName(testData), Is.EqualTo(expected));
+    }
+    */
 }
 
